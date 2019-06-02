84e46daa4942b1bd9c1896c6dd2d9dcb4ad7c6dd
Kasper Hauschildt <kaspervh@protonmail.com>
2019-06-02 20:18:51 +0200

    made the test work

--- spec/root/welcome_spec.rb
+++ spec/root/welcome_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'test root welcome page', type: :feature do
+RSpec.describe 'test root welcome page', type: :feature, js: true do
   before(:each) do
     visit root_path
   end
 
-  scenario 'visit page ', js: true do
+  scenario 'visit page ' do
     expect_page_content(['Jobline', 'Log ind', 'Jobsøgning gjort enkelt', 'Tilmæld som jobsøgende'])
     dont_expect_page_content(['Søg og ansøg på samme tid'])
   end
 
-  scenario 'click the right-btn and see the page changing 3 times', js: true do
+  scenario 'click the right-btn and see the page changing 3 times' do
     3.times { find('.right-btn').click }
     expect_page_content(['Giv sagsbehandleren overblik'])
   end
@@ -28,6 +28,6 @@ RSpec.describe 'test root welcome page', type: :feature do
 
   scenario 'click login button and go to sign in path' do
     click_on 'Log ind'
-    expect(current_path).to eq sign_in_path
+    expect(current_path).to eq login_path
   end
 end
