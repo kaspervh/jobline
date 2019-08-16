76960b0f76ec678ddeca1af9013734515509bd7b
kaspervh <kaspervhauschildt@protonmail.com>
2019-08-16 11:55:07 +0000

    fixed an error message and added a few more features to dashboard

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -14,6 +14,9 @@ class RootController < ApplicationController
     @applications_this_week = current_user.applied_jobs.where(created_at: this_week).count
     @interview_invites_this_month = current_user.applied_jobs.where(application_status_id: 2, updated_at: this_month).count
    	@interview_invites_total = current_user.applied_jobs.where(application_status_id: 2).count
+   	@applications_by_phone = current_user.applied_jobs.where(application_type_id: 1).count
+   	@applications_in_person = current_user.applied_jobs.where(application_type_id: 2).count
+   	@applications_written = current_user.applied_jobs.where(application_type_id: 3).count
     	
   end
 end
