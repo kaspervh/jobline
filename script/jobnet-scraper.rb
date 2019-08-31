50fd513f02ce73b26bcc199218e6dacaec38e3f4
Kasper Hauschildt <kaspervhauschildt@protonmail.com>
2019-08-31 20:50:01 +0200

    got further with the scraper

--- script/jobnet-scraper.rb
+++ script/jobnet-scraper.rb
@@ -2,6 +2,42 @@ require 'watir'
 
 browser = Watir::Browser.new
 
-browser.goto('https://job.jobnet.dk/CV/FindWork/Details/5035220')
+links = [
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Akademisk%2520arbejde", 
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Bygge%2520og%2520anl%25C3%25A6g",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Design,%2520formgivning%2520og%2520grafisk%2520arbejde",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Elever",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Hotel,%2520restauration,%2520k%25C3%25B8kken,%2520kantine%2520",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Industriel%2520produktion",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=It%2520og%2520teleteknik",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Jern,%2520metal%2520og%2520auto",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Kontor,%2520administration,%2520regnskab%2520og%2520finans",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Landbrug,%2520skovbrug,%2520gartneri,%2520fiskeri%2520og%2520dyrepleje%2520",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Ledelse",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Medie,%2520kultur,%2520turisme,%2520idr%25C3%25A6t%2520og%2520underholdning",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=N%25C3%25A6rings-%2520og%2520nydelsesmiddel",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=P%25C3%25A6dagogisk,%2520socialt%2520og%2520kirkeligt%2520arbejde",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Reng%25C3%25B8ring,%2520ejendomsservice%2520og%2520renovation",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Salg,%2520indk%25C3%25B8b%2520og%2520markedsf%25C3%25B8ring",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Sundhed,%2520omsorg%2520og%2520personlig%2520pleje",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Tekstil%2520og%2520bekl%25C3%25A6dning",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Transport,%2520post,%2520lager-%2520og%2520maskinf%25C3%25B8rerarbejde",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Tr%25C3%25A6,%2520m%25C3%25B8bel,%2520glas%2520og%2520keramik",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Undervisning%2520og%2520vejledning",
+         "https://job.jobnet.dk/CV/FindWork?Offset=0&SortValue=BestMatch&OccupationAreas=Vagt,%2520sikkerhed%2520og%2520overv%25C3%25A5gning"
+        ]
+number = 0 
 
-puts browser.element(css: '.white-well').text.strip
+links.each do |link|
+  #Watir.default_timeout = 20
+
+  browser.goto(link.to_s)
+
+  puts browser.element(css: '.qualification-remove.ng-binding').text.strip
+
+  number = number + browser.ul(css: '.list-margin-standard').lis.count
+
+  #puts browser.element(css: '.white-well').text.strip
+end
+
+puts number
