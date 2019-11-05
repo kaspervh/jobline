5ae7719cc5902c51f6f6e7a67788590e144470d2
kaspervh <kaspervhauschildt@protonmail.com>
2019-11-05 14:08:07 +0100

    made a cleaner for job postings

--- script/job_posting_cleaner.rb
+++ script/job_posting_cleaner.rb
@@ -4,31 +4,31 @@
 
 
 # loops through each job posting
-#JobPosting.all.each do |jp|
+JobPosting.all.each do |jp|
   #sanitises the danish date
-#  danish_date = jp.application_deadline.split(' ')
-#  day = danish_date[0].chomp('.')
-#  month = ''
-#  month = '01' if danish_date[1] == 'januar'
-#  month = '02' if danish_date[1] == 'febuar'
-#  month = '03' if danish_date[1] == 'marts'
-#  month = '04' if danish_date[1] == 'april'
-#  month = '05' if danish_date[1] == 'maj'
-#  month = '06' if danish_date[1] == 'juni'
-#  month = '07' if danish_date[1] == 'juli'
-#  month = '08' if danish_date[1] == 'august'
-#  month = '09' if danish_date[1] == 'september'
-#  month = '10' if danish_date[1] == 'oktober'
-#  month = '11' if danish_date[1] == 'november'
-#  month = '12' if danish_date[1] == 'december'
-#  year = danish_date[2]
+  danish_date = jp.application_deadline.split(' ')
+  day = danish_date[0].chomp('.')
+  month = ''
+  month = '01' if danish_date[1] == 'januar'
+  month = '02' if danish_date[1] == 'febuar'
+  month = '03' if danish_date[1] == 'marts'
+  month = '04' if danish_date[1] == 'april'
+  month = '05' if danish_date[1] == 'maj'
+  month = '06' if danish_date[1] == 'juni'
+  month = '07' if danish_date[1] == 'juli'
+  month = '08' if danish_date[1] == 'august'
+  month = '09' if danish_date[1] == 'september'
+  month = '10' if danish_date[1] == 'oktober'
+  month = '11' if danish_date[1] == 'november'
+  month = '12' if danish_date[1] == 'december'
+  year = danish_date[2]
   
-#  deadline = "#{year}-#{month}-#{day}".to_date
+  deadline = "#{year}-#{month}-#{day}".to_date
   
   # checks if the application deadline is 
-#  if deadline < Date.today
-#    jp.destroy
-#  end
+  if deadline < Date.today
+    jp.destroy
+  end
   
-#end
+end
 puts JobPosting.all.count
